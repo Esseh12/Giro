24ceff492cbeb42d7c3ad880d22b80cb8e2f8e54
Esseh12 <gbenmoese788@gmail.com>
2024-09-16 11:11:27 +0100

    404 page done

--- giro/src/App.js
+++ giro/src/App.js
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from "./components/authetication/login";
 import About from './components/mainPages/about';
+import Error404 from './components/mainPages/error404';
 
 function App() {
   return (
     <>
       <Routes >
+        <Route path="/" />
         <Route path='/login' element={<Login />} />
         <Route path='/about' element={<About />} />
+        <Route path='*' element={<Error404 />} />
       </Routes>
     </>
   );
