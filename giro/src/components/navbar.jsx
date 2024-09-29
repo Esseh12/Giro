dec0f3fa265f8f35cce2016475e0e58eb39aa1b2
Esseh12 <gbenmoese788@gmail.com>
2024-09-29 12:14:38 +0100

    homepage dashboard done but not responisive

--- giro/src/components/navbar.jsx
+++ giro/src/components/navbar.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import logo from "../assets/giro_logo.png";
 import { NavLink } from "react-router-dom";
 import "../styles/navbar.css";
 import { IoMdSearch } from "react-icons/io";
 import { FaRegHeart } from "react-icons/fa";
 import { BsCart3 } from "react-icons/bs";
-import { LuUserCircle2 } from "react-icons/lu";
+import { FaRegUser } from "react-icons/fa";
+import { LuShoppingBag } from "react-icons/lu";
+import { FaRegTimesCircle } from "react-icons/fa";
+import { CiStar } from "react-icons/ci";
+import { TbLogout2 } from "react-icons/tb";
 
 const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <>
       <nav id="navbar" className="navbar">
@@ -50,7 +61,40 @@ const Navbar = () => {
           <div className="navbar-icons">
             <FaRegHeart className="navbar-icon" />
             <BsCart3 className="navbar-icon" />
-            <LuUserCircle2 className="navbar-icon" />
+
+            {/* icon with dropdowns */}
+            <div className="user-icon-container">
+              <FaRegUser
+                className={`navbar-icon user-icon ${
+                  isDropdownOpen ? "active" : ""
+                }`}
+                onClick={toggleDropdown}
+              />
+              {isDropdownOpen && (
+                <div className="dropdown-content glass-bg">
+                  <div className="dropdown-content-subcontainer">
+                    <FaRegUser classname="dropdown-icon" />
+                    <a href="#more">Manage My Account</a>
+                  </div>
+                  <div className="dropdown-content-subcontainer">
+                    <LuShoppingBag classname="dropdown-icon" />
+                    <a href="#more">My Order</a>
+                  </div>
+                  <div className="dropdown-content-subcontainer">
+                    <FaRegTimesCircle classname="dropdown-icon" />
+                    <a href="#more">My Cancellations</a>
+                  </div>
+                  <div className="dropdown-content-subcontainer">
+                    <CiStar classname="dropdown-icon" />
+                    <a href="#more">My Reviews</a>
+                  </div>
+                  <div className="dropdown-content-subcontainer">
+                    <TbLogout2 classname="dropdown-icon" />
+                    <a href="#more">Logout</a>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </nav>
