40e2f3e9b7e70294645abaf6c661b00db4f715fc
Esseh12 <gbenmoese788@gmail.com>
2024-10-10 23:43:02 +0100

    update 404 page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./components/mainPages/homepage";
 import Signup from "./components/authetication/signup";
 import About from "./components/mainPages/about";
-import Error404 from "./components/mainPages/error404";
+import Error404 from "./components/mainPages/404";
 import Contact from "./components/mainPages/contact";
 import Signin from "./components/authetication/signin";
 import Profile from "./components/mainPages/profile";
