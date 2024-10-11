7e67c6ce90dce5f402d001f1a8de7f18fcfa16bf
imoleFolio <dremkay71@gmail.com>
2024-10-11 12:31:24 +0100

    image url chnged from jpg to svg and all lowercased

---
+++ backend/url-rename.py
@@ -0,0 +1,26 @@
+from models.db import getDB
+
+
+db = getDB()
+
+cur = db.cursor()
+
+cur.execute('SELECT id, image_url FROM products')
+products = dict(cur.fetchall())
+
+print(products)
+print('-----------------------------------')
+
+
+for key_id, url in products.items():
+  url = url.replace('.jpg', '.svg').lower()
+  cur.execute('UPDATE products SET image_url = ? WHERE id = ?', (url, key_id))
+
+db.commit()
+
+cur.execute('SELECT id, image_url FROM products')
+products = dict(cur.fetchall())
+print(products)
+
+cur.close()
+db.close()
