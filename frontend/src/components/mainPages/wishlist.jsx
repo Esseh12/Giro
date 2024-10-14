a85769a927651679bb2ec42f37fa0d35f503cd0c
Esseh12 <gbenmoese788@gmail.com>
2024-10-15 00:43:15 +0100

    wishlist component 60% done

--- frontend/src/components/mainPages/wishlist.jsx
+++ frontend/src/components/mainPages/wishlist.jsx
@@ -1,30 +1,81 @@
 import { useContext } from "react";
 import { WishlistContext } from "./Homepage/wishlistContext";
+import { CartContext } from "./Homepage/cartContext";
+import { BsTrash3 } from "react-icons/bs";
+import Top from "../top";
+import Navbar from "../navbar";
+import Footer from "../footer";
+import "../../styles/homepage.css";
+import "../../styles/wishlist.css";
 
 const WishList = () => {
   const { wishlistItems, removeFromWishlist, getTotalWishlistItems } =
     useContext(WishlistContext);
 
-  return (
-    <div>
-      <h2>WishList</h2>
-      <p>Total Wishlist Items: {getTotalWishlistItems()}</p>
+  const { addToCart } = useContext(CartContext); // Get the addToCart function from the context
 
-      {wishlistItems.length === 0 ? (
-        <p>Your wishlist is empty.</p>
-      ) : (
-        <ul>
-          {wishlistItems.map((item) => (
-            <li key={item.id}>
-              {item.name}
-              <button onClick={() => removeFromWishlist(item.id)}>
-                Remove
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+  return (
+    <>
+      <Top />
+      <Navbar />
+      {/* wishlist main container */}
+      <div>
+        {/* wishlist */}
+        <section>
+          <div>
+            <p>Wishlist ({getTotalWishlistItems()})</p>
+            <button>Move All To Cart</button>
+          </div>
+          <div>
+            {wishlistItems.length === 0 ? (
+              <p>Your wishlist is empty.</p>
+            ) : (
+              <ul className="wishlist-items">
+                {wishlistItems.map((item) => (
+                  <li key={item.id}>
+                    <div className="flashsales-single-image wishlist-single-image">
+                      <img
+                        src={`/products/${item.image_url}`}
+                        alt={item.name}
+                        className="wishlist-img"
+                      />
+                      <div className="flashsales-icon-container wishlist-icon-container">
+                        <div className="flashsales-icon-background wishlist-icon-background">
+                          <BsTrash3
+                            onClick={() => removeFromWishlist(item.id)}
+                            className="flashsales-icon wishlist-icon"
+                          />
+                        </div>
+                      </div>
+                      <div className="flashsales-cart-option">
+                        <p
+                          className="flashsales-cart-option-text"
+                          onClick={() => addToCart(item)}
+                        >
+                          Add To Cart
+                        </p>
+                      </div>
+                    </div>
+                    <div className="wishlist-details">
+                      <h4 className="flashsale-product-name">{item.name}</h4>
+                      <p className="discounted-price">${item.price}</p>
+                    </div>
+                    {/* {item.name} */}
+                    {/* <img src={`/products/${item.image_url}`} alt="" /> */}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </section>
+        {/* just for you section */}
+        <section>
+          <div></div>
+          <div></div>
+        </section>
+      </div>
+      <Footer />
+    </>
   );
 };
 
